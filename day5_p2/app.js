3355097bb6f0981f1f686e1d5adb74c962d3c37f
kshipra-jadav <jadavkshipra2@gmail.com>
2022-12-05 14:14:43 +0530

    add day 5 part 2 solution

---
+++ day5_p2/app.js
@@ -0,0 +1,36 @@
+import * as fs from 'fs/promises'
+
+const allItems = [
+	['T', 'P', 'Z', 'C', 'S', 'L', 'Q', 'N'],
+	['L', 'P', 'T', 'V', 'H', 'C', 'G'],
+	['D', 'C', 'Z', 'F'],
+	['G', 'W', 'T', 'D', 'L', 'M', 'V', 'C'],
+	['P', 'W', 'C'],
+	['P', 'F', 'J', 'D', 'C', 'T', 'S', 'Z'],
+	['V', 'W', 'G', 'B', 'D'],
+	['N', 'J', 'S', 'Q', 'H', 'W'],
+	['R', 'C', 'Q', 'F', 'S', 'L', 'V']
+]
+const input = await fs.readFile('input.txt', 'utf-8')
+
+const allMoves = input.match(/\d+/gm)
+
+for(let i = 0; i <= allMoves.length - 3; i += 3) {
+	let numberOfItems = allMoves[i]
+	let fromStack = allMoves[i+1]
+	let toStack = allMoves[i+2]
+	
+	let poppedItems = []
+	for(let i = 1; i <= numberOfItems; i ++) {
+		let currentItem = allItems[fromStack-1].pop()
+		poppedItems.push(currentItem)
+	}
+	poppedItems.reverse()
+	poppedItems.map(item => allItems[toStack-1].push(item))
+}
+
+const topItems = []
+allItems.forEach(item => {
+	topItems.push(item.pop())
+})
+console.log(topItems.join(''))
